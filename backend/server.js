88adf9ba314f0f9081ea8f9af85f51b8be21be3b
Sarsande-Sunil <priyankaingle250@gmail.com>
2023-03-12 12:46:54 +0530

    aaded

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,17 @@ const cors = require("cors");
 
 const app = express();
 
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+//Routes
+app.get("/", (req, res) => {
+    res.send("Home Page")
+})
+
 const PORT = process.env.PORT || 5000
 
 //Connected to DB server
