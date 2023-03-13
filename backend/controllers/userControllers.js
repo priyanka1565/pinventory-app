ae579d887b35cf1037dc67c0076bcbe7481a8a4f
Sarsande-Sunil <priyankaingle250@gmail.com>
2023-03-13 18:09:17 +0530

    token generation problem is fixed

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -3,51 +3,55 @@ const User = require("../models/useModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-//jwt token
+// Generate Token
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET,{expiresIn:"1d"});
-  
-}
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+};
+
 
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body
-  
+  const { name, email, password } = req.body;
+
   //Validation
   if (!name || !email || !password) {
-    res.status(400)
-    throw new Error("Please fill in all required fields")
+    res.status(400);
+    throw new Error("Please fill in all required fields");
   }
   if (password.length < 6) {
     res.status(400);
     throw new Error("Password must be 6 characters");
-    
   }
 
   //Check if user email already exists
 
-  const userExists = await User.findOne({ email })
-  
+  const userExists = await User.findOne({ email });
+
   if (userExists) {
-     res.status(400);
-     throw new Error("Email has already been register");
-    
-    
+    res.status(400);
+    throw new Error("Email has already been register");
   }
-//Generate Token
-  
-  const token = generateToken(user._id)
- 
+
   // Create new user
   const user = await User.create({
     name,
     email,
     password,
-    
+  });
 
-  })
+  //Generate Token
+  const token = generateToken(user._id);
+
+  // Send HTTP-only cookie
+  res.cookie("token", token, {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000 * 86400), // 1 day
+    sameSite: "none",
+    secure: true,
+  });
 
   if (user) {
-    const { _id, name, email, photo, phone, bio,token } = user;
+    const { _id, name, email, photo, phone, bio } = user;
     res.status(201).json({
       _id,
       name,
@@ -57,12 +61,10 @@ const registerUser = asyncHandler(async (req, res) => {
       bio,
       token,
     });
-    
-  } else{
-    res.status(400)
-    throw new Error("Invalid user data")
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
   }
-    
 })
 
 
